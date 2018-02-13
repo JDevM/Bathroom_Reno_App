540a6d140fc6459f42805777fcdd8f69eed028ed
JDevM <jaredsguitarlessons@gmail.com>
2018-02-13 12:43:59 -0500

    First Commit

---
+++ JavaScript/script.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+    
+var permits_Button = document.getElementById("Permits_Button"),
+    demo_Button = document.getElementById("Demo_Button"),
+    reconstruction_Button = document.getElementById("Reconstruction_Button"),
+    cabinetry_Button = document.getElementById("Cabinetry_Button"),
+    tops_Button = document.getElementById("Tops_Button"),
+    sink_Bowls_Button = document.getElementById("Sink_Bowls_Button"),
+    tile_Button = document.getElementById("Tile_Button"),
+    glass_Shower_Enclosures_Button = document.getElementById("Glass_Shower_Enclosures_Button"),
+    plumbing_Button = document.getElementById("Plumbing_Button"),
+    electrical_Button = document.getElementById("Electrical_Button"),
+    hvac_Button = document.getElementById("HVAC_Button"),
+    custom_Trimwork_Button = document.getElementById("Custom_Trimwork_Button"),
+    custom_Mirrors_Button = document.getElementById("Custom_Mirrors_Button"),
+    painting_Button = document.getElementById("Painting_Button");
+    
+    
+// Tests for input and button comunication
+    
+    permits_Button.onclick = function () {
+        console.log("Permits button has been clicked");
+        var input = document.getElementById("Permits_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    demo_Button.onclick = function () {
+        console.log("Demo button has been clicked");
+        var input = document.getElementById("Demo_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    reconstruction_Button.onclick = function () {
+        console.log("Reconstruction button has been clicked");
+        var input = document.getElementById("Reconstruction_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    cabinetry_Button.onclick = function () {
+        console.log("Cabinetry button has been clicked");
+        var input = document.getElementById("Cabinetry_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    tops_Button.onclick = function () {
+        console.log("Tops button has been clicked");
+        var input = document.getElementById("Tops_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    sink_Bowls_Button.onclick = function () {
+        console.log("Sink Bowls button has been clicked");
+        var input = document.getElementById("Sink_Bowls_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    tile_Button.onclick = function () {
+        console.log("Tile button has been clicked");
+        var input = document.getElementById("Tile_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    glass_Shower_Enclosures_Button.onclick = function () {
+        console.log("Glass Shower Eclosures button has been clicked");
+        var input = document.getElementById("Glass_Shower_Enclosures_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    plumbing_Button.onclick = function () {
+        console.log("Plumbing button has been clicked");
+        var input = document.getElementById("Plumbing_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    electrical_Button.onclick = function () {
+        console.log("Electrical button has been clicked");
+        var input = document.getElementById("Electrical_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    hvac_Button.onclick = function () {
+        console.log("HVAC button has been clicked");
+        var input = document.getElementById("HVAC_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    custom_Trimwork_Button.onclick = function () {
+        console.log("Custom Trimwork button has been clicked");
+        var input = document.getElementById("Custom_Trimwork_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    custom_Mirrors_Button.onclick = function () {
+        console.log("Custom Mirrors button has been clicked");
+        var input = document.getElementById("Custom_Mirrors_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    painting_Button.onclick = function () {
+        console.log("Painting button has been clicked");
+        var input = document.getElementById("Painting_Entry").value;
+        console.log("Your input was " + input);
+    };
+    
+    
+});
